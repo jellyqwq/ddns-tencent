77b507790db453b336a9703dc247bbab0816bccb
SweetJellyQwQ <577430840@qq.com>
2024-02-13 23:16:59 +0800

    support ipv4 ddns

--- DDNS-Tencent.go
+++ DDNS-Tencent.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"os"
@@ -14,37 +15,89 @@ import (
 	dnspod "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/dnspod/v20210323"
 )
 
-var (
-	SecretId  string
-	SecretKey string
-
+// 定义一个type
+type DDNSproperty struct {
 	Domain     string
 	SubDomain  string
 	RecordType string
 	RecordLine string
-	// Value          string
-	RecordId string
+	Value      string
+	RecordId   string
+}
 
-	CronExpression string
+var (
+	SecretId       string
+	SecretKey      string
+	CronExpression string = "*/5 * * * *"
+
+	ipv4Property *DDNSproperty
+	ipv6Property *DDNSproperty
 )
 
 func init() {
-	for _, value := range []string{"SecretId", "SecretKey", "Domain", "SubDomain", "RecordType", "RecordLine", "RecordId", "CronExpression"} {
-		if os.Getenv(value) == "" {
-			log.Panicf("%s is empty.", value)
+	// 先检查有无 SecretId和SecretKey, 没有则退出, 有则写入全局变量
+	for _, value := range []string{"SecretId", "SecretKey"} {
+		tempVar := os.Getenv(value)
+		if tempVar == "" {
+			log.Panicf("%s cannot be empty.", value)
 		}
+		if value == "SecretId" {
+			SecretId = tempVar
+		} else {
+			SecretKey = tempVar
+		}
+	}
+
+	// 是否需要覆盖定时执行
+	tempVar := os.Getenv("CronExpression")
+	if tempVar != "" {
+		CronExpression = tempVar
+	}
+
+	// 检查是否有 Ipv<4|6>Domain 参数, 没有则退出
+	for _, value := range []string{"Ipv4", "Ipv6"} {
+		if os.Getenv(fmt.Sprintf("%sDomain", value)) == "" {
+			log.Printf("Skip %s ", value)
+			continue
+		} else if value == "Ipv4" {
+			ipv4Property = NewDDNSproperty()
+			ipv4Property.Domain = os.Getenv(fmt.Sprintf("%sDomain", value))
+			ipv4Property.SubDomain = os.Getenv(fmt.Sprintf("%sSubDomain", value))
+			if os.Getenv(fmt.Sprintf("%sRecordLine", value)) != "" {
+				ipv4Property.RecordLine = os.Getenv(fmt.Sprintf("%sRecordLine", value))
+			}
+			ipv4Property.RecordType = "A"
+			ipv4Property.RecordId = os.Getenv(fmt.Sprintf("%sRecordId", value))
+		} else {
+			ipv6Property = NewDDNSproperty()
+			ipv6Property.Domain = os.Getenv(fmt.Sprintf("%sDomain", value))
+			ipv6Property.SubDomain = os.Getenv(fmt.Sprintf("%sSubDomain", value))
+			if os.Getenv(fmt.Sprintf("%sRecordLine", value)) != "" {
+				ipv6Property.RecordLine = os.Getenv(fmt.Sprintf("%sRecordLine", value))
+			}
+			ipv6Property.RecordType = "AAAA"
+			ipv6Property.RecordId = os.Getenv(fmt.Sprintf("%sRecordId", value))
+		}
+	}
+
+	if ipv4Property.Domain == "" && ipv6Property.Domain == "" {
+		log.Panic("No service need to run.")
 	}
 }
 
-func ModifyRecord(ipv6_addr string) {
+func NewDDNSproperty() *DDNSproperty {
+	return &DDNSproperty{
+		RecordLine: "默认",
+	}
+}
+
+func (property *DDNSproperty) ModifyRecord() {
 	// 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
 	// 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
 	// 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
 	credential := common.NewCredential(
-		// "SecretId",
-		os.Getenv("SecretId"),
-		// "SecretKey",
-		os.Getenv("SecretKey"),
+		SecretId,
+		SecretKey,
 	)
 	// 实例化一个client选项，可选的，没有特殊需求可以跳过
 	cpf := profile.NewClientProfile()
@@ -55,12 +108,12 @@ func ModifyRecord(ipv6_addr string) {
 	// 实例化一个请求对象,每个接口都会对应一个request对象
 	request := dnspod.NewModifyRecordRequest()
 
-	request.Domain = common.StringPtr(os.Getenv("Domain"))
-	request.SubDomain = common.StringPtr(os.Getenv("SubDomain"))
-	request.RecordType = common.StringPtr(os.Getenv("RecordType"))
-	request.RecordLine = common.StringPtr(os.Getenv("RecordLine"))
-	request.Value = common.StringPtr(ipv6_addr)
-	ui64, err := strconv.ParseUint(os.Getenv("RecordId"), 10, 64)
+	request.Domain = common.StringPtr(property.Domain)
+	request.SubDomain = common.StringPtr(property.SubDomain)
+	request.RecordType = common.StringPtr(property.RecordType)
+	request.RecordLine = common.StringPtr(property.RecordLine)
+	request.Value = common.StringPtr(property.Value)
+	ui64, err := strconv.ParseUint(property.RecordId, 10, 64)
 	if err != nil {
 		log.Panicln(err)
 	}
@@ -86,6 +139,7 @@ func task() {
 		log.Panic(err)
 	}
 
+	ipv4 := ""
 	ipv6 := ""
 
 	for _, iface := range ifaces {
@@ -99,21 +153,42 @@ func task() {
 			// fmt.Printf("IP Address: %v %v %v\n", addr, IPN.IP.IsPrivate(), IPN.IP.IsGlobalUnicast())
 			if !IPN.IP.IsPrivate() && IPN.IP.IsGlobalUnicast() {
 				// 如果是公网ip(非私有地址) 且是广播地址的网络
-				log.Println("IPV6 Address: ", IPN.IP.To16().String())
-				ipv6 = IPN.IP.To16().String()
+				if IPN.IP.To4() != nil {
+					ipv4 = IPN.IP.To4().String()
+					if ipv4Property.Domain != "" && ipv6Property.Domain == "" {
+						log.Println("IPV4 Address: ", ipv4)
+						ipv4Property.Value = ipv4
+						ipv4Property.ModifyRecord()
+						return
+					}
+				} else if IPN.IP.To16() != nil {
+					ipv6 = IPN.IP.To16().String()
+					if ipv6Property.Domain != "" && ipv4Property.Domain == "" {
+						log.Println("IPV6 Address: ", ipv6)
+						ipv6Property.Value = ipv6
+						ipv6Property.ModifyRecord()
+						return
+					}
+				}
 			}
 		}
-		if ipv6 != "" {
-			break
+
+		// 两个都找到了提前结束寻找
+		if ipv4 != "" && ipv6 != "" {
+			log.Println("IPV4 Address: ", ipv4)
+			ipv4Property.Value = ipv4
+			ipv4Property.ModifyRecord()
+
+			log.Println("IPV6 Address: ", ipv6)
+			ipv6Property.Value = ipv6
+			ipv6Property.ModifyRecord()
+
+			return
 		}
 	}
-
-	ModifyRecord(ipv6)
 }
 
 func main() {
-	// task()
-
 	// 定时
 	c := cron.New()
 	_, err := c.AddFunc(os.Getenv("CronExpression"), func() {
--- README.md
+++ README.md
@@ -1,8 +1,41 @@
 # ddns-tencent
 
-# Usage
-Write `docker-compose.yml` environment and run
+## 用法
+编辑 `docker-compose.yml` 环境变量然后运行
 ```sh
 docker compose up -d
+```
+
+## 环境变量说明
+
+|变量名|说明
+|-|-|
+|Ipv4Domain|ipv4要解析的域名, 若为空或无该字段则Ipv4的字段均不生效|
+|Ipv4SubDomain|ipv4子域名|
+|Ipv4RecordLine|某条线路名称的解析记录, 选填
+|Ipv4RecordId|记录Id, 详见 <https://cloud.tencent.com/document/api/1427/56166>
+|Ipv6Domain|ipv6要解析的域名, 若为空或无该字段则Ipv6的字段均不生效|
+|Ipv6SubDomain|ipv6子域名|
+|Ipv6RecordLine|某条线路名称的解析记录, 选填
+|Ipv6RecordId|记录Id, 详见 <https://cloud.tencent.com/document/api/1427/56166>
+|SecretId|详见 <https://console.dnspod.cn/account/token/apikey>|
+|SecretKey|详见 <https://console.dnspod.cn/account/token/apikey>|
+|CronExpression|定时任务表达式, 默认值为 `*/5 * * * *`, 选填
+
+## 例子
+只对ipv6的配置ddns, 该配置会将ipv6解析到 `ddns.test.example.com` 上 
+```yaml
+services:
+  ddns:
+    image: jellyqwq/ddns-tencent:latest
+    container_name: ddns-tencent
+    restart: unless-stopped
+    network_mode: host
+    environment:
+      - Ipv6Domain=example.com
+      - Ipv6SubDomain=ddns.test
+      - Ipv6RecordId=xxxxxxxx
+      - SecretId=xxxxxxxxxxxxxxxxxxx
+      - SecretKey=xxxxxxxxxxxxxxxxxx
 ```
 
